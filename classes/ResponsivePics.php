de670c9b92844e36c8d1e75a1646cb8fde925b5c
Enrique Balleste <eballeste@divisionof.com>
2020-02-15 22:27:12 -0500

    fixes non-cropped image when factor ratio given

--- classes/ResponsivePics.php
+++ classes/ResponsivePics.php
@@ -634,8 +634,11 @@ if (!class_exists('ResponsivePics')) {
 					$ratio = $original_width / $original_height;
 
 					if ($crop_ratio) {
-						$resized_url = self::get_resized_url($id, $file_path, $url, $original_width, $original_width * $crop_ratio, $crop);
 						$ratio       = $original_width / ($original_width * $crop_ratio);
+						$new_height  = $original_width * $crop_ratio;
+						// check if new height will be enough to get the right aspect ratio
+						$new_width   = $new_height <= $original_height ? $original_width : $original_height * $ratio;
+						$resized_url = self::get_resized_url($id, $file_path, $url, $new_width, $new_height, $crop);
 					}
 
 					$source1x   = isset($resized_url) ? $resized_url : $url;
