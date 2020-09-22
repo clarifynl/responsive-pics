82b2172fce882c1b9abf4641145ee5f6cd73d72e
Twansparant <toinekamps@gmail.com>
2020-09-22 13:39:10 +0200

    Use parent class in error

--- src/class-error.php
+++ src/class-error.php
@@ -4,7 +4,7 @@ class RP_Error extends ResponsivePics {
 
 	// print error message
 	public function show_error($message) {
-		$error = sprintf('<pre>%s error: %s</pre>', get_class(), $message);
+		$error = sprintf('<pre>%s error: %s</pre>', get_parent_class(), $message);
 
 		echo $error;
 	}
