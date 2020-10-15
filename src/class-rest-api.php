e6b9bdcc7c30400a9ec9bb51697e20bee22d9033
Twansparant <toinekamps@gmail.com>
2020-10-15 17:01:32 +0200

    Add sanitize_callbacks

--- src/class-rest-api.php
+++ src/class-rest-api.php
@@ -4,41 +4,122 @@ class RP_Rest_Api extends ResponsivePics {
 
 	public static function register_api_routes() {
 		// picture
-		register_rest_route('responsive-pics', '/v1/get', [
+		register_rest_route('responsive-pics/v1', '/get(-picture)?/(?P<id>\d+)/(?P<sizes>\S+)(?:/(?P<classes>\S+))?', [
 			'methods'             => 'GET',
 			'callback'            => ['RP_Rest_Api', 'rest_get_picture'],
-			'permission_callback' => '__return_true'
-		]);
-
-		register_rest_route('responsive-pics', '/v1/get-picture', [
-			'methods'             => 'GET',
-			'callback'            => ['RP_Rest_Api', 'rest_get_picture'],
-			'permission_callback' => '__return_true'
+			'permission_callback' => '__return_true',
+			'args'                => [
+				'id' => [
+					'validate_callback' => function($id, $request, $key) {
+						return is_numeric($id);
+					},
+					'sanitize_callback' => function($id, $request, $key) {
+						return (int) $id;
+					}
+				],
+				'sizes' => [
+					'validate_callback' => function($sizes, $request, $key) {
+						return is_string($sizes);
+					},
+					'sanitize_callback' => function($sizes, $request, $key) {
+						return urldecode($sizes);
+					}
+				],
+				'classes' => [
+					'validate_callback' => function($classes, $request, $key) {
+						return is_string($classes);
+					},
+					'sanitize_callback' => function($classes, $request, $key) {
+						return urldecode($classes);
+					}
+				]
+			]
 		]);
 
 		// image
-		register_rest_route('responsive-pics', '/v1/get-image', [
+		register_rest_route('responsive-pics/v1', '/get-image/(?P<id>\d+)/(?P<sizes>\S+)(?:/(?P<crop>\S+))?(?:/(?P<classes>\S+))?', [
 			'methods'             => 'GET',
 			'callback'            => ['RP_Rest_Api', 'rest_get_image'],
-			'permission_callback' => '__return_true'
+			'permission_callback' => '__return_true',
+			'args'                => [
+				'id' => [
+					'validate_callback' => function($id, $request, $key) {
+						return is_numeric($id);
+					},
+					'sanitize_callback' => function($id, $request, $key) {
+						return (int) $id;
+					}
+				],
+				'sizes' => [
+					'validate_callback' => function($sizes, $request, $key) {
+						return is_string($sizes);
+					},
+					'sanitize_callback' => function($sizes, $request, $key) {
+						return urldecode($sizes);
+					}
+				],
+				'crop' => [
+					'validate_callback' => function($crop, $request, $key) {
+						return is_string($crop) || is_numeric($crop);
+					},
+					'sanitize_callback' => function($crop, $request, $key) {
+						return urldecode($crop);
+					}
+				]
+				'classes' => [
+					'validate_callback' => function($classes, $request, $key) {
+						return is_string($classes);
+					},
+					'sanitize_callback' => function($classes, $request, $key) {
+						return urldecode($classes);
+					}
+				]
+			]
 		]);
 
 		// background
-		register_rest_route('responsive-pics', '/v1/get-background', [
+		register_rest_route('responsive-pics/v1', '/get-background/(?P<id>\d+)/(?P<sizes>\S+)(?:/(?P<classes>\S+))?', [
 			'methods'             => 'GET',
 			'callback'            => ['RP_Rest_Api', 'rest_get_background'],
-			'permission_callback' => '__return_true'
+			'permission_callback' => '__return_true',
+			'args'                => [
+				'id' => [
+					'validate_callback' => function($id, $request, $key) {
+						return is_numeric($id);
+					},
+					'sanitize_callback' => function($id, $request, $key) {
+						return (int) $id;
+					}
+				],
+				'sizes' => [
+					'validate_callback' => function($sizes, $request, $key) {
+						return is_string($sizes);
+					},
+					'sanitize_callback' => function($sizes, $request, $key) {
+						return urldecode($sizes);
+					}
+				],
+				'classes' => [
+					'validate_callback' => function($classes, $request, $key) {
+						return is_string($classes);
+					},
+					'sanitize_callback' => function($classes, $request, $key) {
+						return urldecode($classes);
+					}
+				]
+			]
 		]);
 	}
 
 	// get picture
 	public static function rest_get_picture($request) {
-		$data      = $request->get_params();
-		$id        = isset($data['id']) ? (int) $data['id'] : null;
-		$sizes     = isset($data['sizes']) ? urldecode($data['sizes']) : null;
-		$classes   = isset($data['classes']) ? urldecode($data['classes']) : null;
-		$lazyload  = isset($data['lazyload']) ? ($data['lazyload'] === 'true') : null;
-		$intrinsic = isset($data['intrinsic']) ? ($data['intrinsic'] === 'true') : null;
+		$id      = isset($request['id']) ? (int) $request['id'] : null;
+		$sizes   = isset($request['sizes']) ? $request['sizes'] : null;
+		$classes = isset($request['classes']) ? $request['classes'] : null;
+
+		$params    = $request->get_params();
+		$lazyload  = isset($params['lazyload']) ? ($params['lazyload'] === 'true') : null;
+		$intrinsic = isset($params['intrinsic']) ? ($params['intrinsic'] === 'true') : null;
 
 		if (class_exists('ResponsivePics')) {
 			if ($id && $sizes) {
@@ -54,7 +135,7 @@ class RP_Rest_Api extends ResponsivePics {
 					return $result;
 				}
 			} else {
-				return new WP_Error('responsive_pics_invalid', __('the request is missing required parameters', 'responsive-pics'), $data);
+				return new WP_Error('responsive_pics_invalid', __('the request is missing required parameters', 'responsive-pics'), $params);
 			}
 		} else {
 			return new WP_Error('responsive_pics_missing', __('the responsive pics plugin was not found', 'responsive-pics'));
@@ -63,12 +144,13 @@ class RP_Rest_Api extends ResponsivePics {
 
 	// get image
 	public static function rest_get_image($request) {
-		$data      = $request->get_params();
-		$id        = isset($data['id']) ? (int) $data['id'] : null;
-		$sizes     = isset($data['sizes']) ? urldecode($data['sizes']) : null;
-		$crop      = isset($data['crop']) ? urldecode($data['crop']) : null;
-		$classes   = isset($data['classes']) ? urldecode($data['classes']) : null;
-		$lazyload  = isset($data['lazyload']) ? ($data['lazyload'] === 'true') : null;
+		$id    = isset($request['id']) ? (int) $request['id'] : null;
+		$sizes = isset($request['sizes']) ? $request['sizes'] : null;
+		$classes = isset($request['classes']) ? $request['classes'] : null;
+
+		$params    = $request->get_params();
+		$crop      = isset($params['crop']) ? urldecode($params['crop']) : null;
+		$lazyload  = isset($params['lazyload']) ? ($params['lazyload'] === 'true') : null;
 
 		if (class_exists('ResponsivePics')) {
 			if ($id && $sizes) {
@@ -84,7 +166,7 @@ class RP_Rest_Api extends ResponsivePics {
 					return $result;
 				}
 			} else {
-				return new WP_Error('responsive_pics_invalid',  __('the request is missing required parameters', 'responsive-pics'), $data);
+				return new WP_Error('responsive_pics_invalid',  __('the request is missing required parameters', 'responsive-pics'), $params);
 			}
 		} else {
 			return new WP_Error('responsive_pics_missing', __('the responsive pics plugin was not found', 'responsive-pics'));
@@ -93,10 +175,9 @@ class RP_Rest_Api extends ResponsivePics {
 
 	// get background
 	public static function rest_get_background($request) {
-		$data    = $request->get_params();
-		$id      = isset($data['id']) ? (int) $data['id'] : null;
-		$sizes   = isset($data['sizes']) ? urldecode($data['sizes']) : null;
-		$classes = isset($data['classes']) ? urldecode($data['classes']) : null;
+		$id      = isset($request['id']) ? (int) $request['id'] : null;
+		$sizes   = isset($request['sizes']) ? $request['sizes'] : null;
+		$classes = isset($request['classes']) ? $request['classes'] : null;
 
 		if (class_exists('ResponsivePics')) {
 			if ($id && $sizes) {
@@ -112,7 +193,7 @@ class RP_Rest_Api extends ResponsivePics {
 					return $result;
 				}
 			} else {
-				return new WP_Error('responsive_pics_invalid',  __('the request is missing required parameters', 'responsive-pics'), $data);
+				return new WP_Error('responsive_pics_invalid',  __('the request is missing required parameters', 'responsive-pics'), $request);
 			}
 		} else {
 			return new WP_Error('responsive_pics_missing', __('the responsive pics plugin was not found', 'responsive-pics'));
