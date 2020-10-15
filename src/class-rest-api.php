54d6325175a05693a0905c3b64a5fb4bb29c14f8
Twansparant <toinekamps@gmail.com>
2020-10-15 17:40:04 +0200

    Only add id as endpoint parameter

--- src/class-rest-api.php
+++ src/class-rest-api.php
@@ -4,7 +4,7 @@ class RP_Rest_Api extends ResponsivePics {
 
 	public static function register_api_routes() {
 		// picture
-		register_rest_route('responsive-pics/v1', '/get(-picture)?/(?P<id>\d+)/(?P<sizes>\S+)(?:/(?P<classes>\S+))?', [
+		register_rest_route('responsive-pics/v1', '/get(-picture)?/(?P<id>\d+)', [
 			'methods'             => 'GET',
 			'callback'            => ['RP_Rest_Api', 'rest_get_picture'],
 			'permission_callback' => '__return_true',
@@ -16,28 +16,12 @@ class RP_Rest_Api extends ResponsivePics {
 					'sanitize_callback' => function($id, $request, $key) {
 						return (int) $id;
 					}
-				],
-				'sizes' => [
-					'validate_callback' => function($sizes, $request, $key) {
-						return is_string($sizes);
-					},
-					'sanitize_callback' => function($sizes, $request, $key) {
-						return urldecode($sizes);
-					}
-				],
-				'classes' => [
-					'validate_callback' => function($classes, $request, $key) {
-						return is_string($classes);
-					},
-					'sanitize_callback' => function($classes, $request, $key) {
-						return urldecode($classes);
-					}
 				]
 			]
 		]);
 
 		// image
-		register_rest_route('responsive-pics/v1', '/get-image/(?P<id>\d+)/(?P<sizes>\S+)(?:/(?P<crop>\S+))?(?:/(?P<classes>\S+))?', [
+		register_rest_route('responsive-pics/v1', '/get-image/(?P<id>\d+)', [
 			'methods'             => 'GET',
 			'callback'            => ['RP_Rest_Api', 'rest_get_image'],
 			'permission_callback' => '__return_true',
@@ -49,36 +33,12 @@ class RP_Rest_Api extends ResponsivePics {
 					'sanitize_callback' => function($id, $request, $key) {
 						return (int) $id;
 					}
-				],
-				'sizes' => [
-					'validate_callback' => function($sizes, $request, $key) {
-						return is_string($sizes);
-					},
-					'sanitize_callback' => function($sizes, $request, $key) {
-						return urldecode($sizes);
-					}
-				],
-				'crop' => [
-					'validate_callback' => function($crop, $request, $key) {
-						return is_string($crop) || is_numeric($crop);
-					},
-					'sanitize_callback' => function($crop, $request, $key) {
-						return urldecode($crop);
-					}
-				],
-				'classes' => [
-					'validate_callback' => function($classes, $request, $key) {
-						return is_string($classes);
-					},
-					'sanitize_callback' => function($classes, $request, $key) {
-						return urldecode($classes);
-					}
 				]
 			]
 		]);
 
 		// background
-		register_rest_route('responsive-pics/v1', '/get-background/(?P<id>\d+)/(?P<sizes>\S+)(?:/(?P<classes>\S+))?', [
+		register_rest_route('responsive-pics/v1', '/get-background/(?P<id>\d+)', [
 			'methods'             => 'GET',
 			'callback'            => ['RP_Rest_Api', 'rest_get_background'],
 			'permission_callback' => '__return_true',
@@ -90,22 +50,6 @@ class RP_Rest_Api extends ResponsivePics {
 					'sanitize_callback' => function($id, $request, $key) {
 						return (int) $id;
 					}
-				],
-				'sizes' => [
-					'validate_callback' => function($sizes, $request, $key) {
-						return is_string($sizes);
-					},
-					'sanitize_callback' => function($sizes, $request, $key) {
-						return urldecode($sizes);
-					}
-				],
-				'classes' => [
-					'validate_callback' => function($classes, $request, $key) {
-						return is_string($classes);
-					},
-					'sanitize_callback' => function($classes, $request, $key) {
-						return urldecode($classes);
-					}
 				]
 			]
 		]);
@@ -113,11 +57,10 @@ class RP_Rest_Api extends ResponsivePics {
 
 	// get picture
 	public static function rest_get_picture($request) {
-		$id      = isset($request['id']) ? (int) $request['id'] : null;
-		$sizes   = isset($request['sizes']) ? $request['sizes'] : null;
-		$classes = isset($request['classes']) ? $request['classes'] : null;
-
+		$id        = isset($request['id']) ? $request['id'] : null;
 		$params    = $request->get_params();
+		$sizes     = isset($params['sizes']) ? urldecode($params['sizes']) : null;
+		$classes   = isset($params['classes']) ? urldecode($params['classes']) : null;
 		$lazyload  = isset($params['lazyload']) ? ($params['lazyload'] === 'true') : null;
 		$intrinsic = isset($params['intrinsic']) ? ($params['intrinsic'] === 'true') : null;
 
@@ -144,11 +87,10 @@ class RP_Rest_Api extends ResponsivePics {
 
 	// get image
 	public static function rest_get_image($request) {
-		$id    = isset($request['id']) ? (int) $request['id'] : null;
-		$sizes = isset($request['sizes']) ? $request['sizes'] : null;
-		$classes = isset($request['classes']) ? $request['classes'] : null;
-
+		$id        = isset($request['id']) ? $request['id'] : null;
 		$params    = $request->get_params();
+		$sizes     = isset($params['sizes']) ? urldecode($params['sizes']) : null;
+		$classes   = isset($params['classes']) ? urldecode($params['classes']) : null;
 		$crop      = isset($params['crop']) ? urldecode($params['crop']) : null;
 		$lazyload  = isset($params['lazyload']) ? ($params['lazyload'] === 'true') : null;
 
@@ -175,9 +117,10 @@ class RP_Rest_Api extends ResponsivePics {
 
 	// get background
 	public static function rest_get_background($request) {
-		$id      = isset($request['id']) ? (int) $request['id'] : null;
-		$sizes   = isset($request['sizes']) ? $request['sizes'] : null;
-		$classes = isset($request['classes']) ? $request['classes'] : null;
+		$id      = isset($request['id']) ? $request['id'] : null;
+		$params  = $request->get_params();
+		$sizes   = isset($params['sizes']) ? urldecode($params['sizes']) : null;
+		$classes = isset($params['classes']) ? urldecode($params['classes']) : null;
 
 		if (class_exists('ResponsivePics')) {
 			if ($id && $sizes) {
