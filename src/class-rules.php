47e1b003cc374777077d92354c73ae74b95ef9cc
Twansparant <toinekamps@gmail.com>
2020-10-28 13:21:16 +0100

    Merge get_art_image_rules with get_image_rules

--- src/class-rules.php
+++ src/class-rules.php
@@ -3,9 +3,15 @@
 class RP_Rules extends ResponsivePics {
 
 	// this processes our resizing syntax with art direction support and returns a normalized array with resizing rules
-	public function get_art_image_rules($input) {
+	public function get_image_rules($input, $art_direction = true, $img_crop = null) {
 		$variants = ResponsivePics()->breakpoints->add_missing_breakpoints(explode(',', $input));
 		$result   = [];
+
+		// get image crop & ratio
+		$global_crop = null;
+		if (!$art_direction && $img_crop) {
+			$global_crop = ResponsivePics()->process->process_ratio_crop($img_crop);
+		}
 
 		foreach ($variants as $variant) {
 			$variant    = trim($variant);
@@ -53,91 +59,9 @@ class RP_Rules extends ResponsivePics {
 				$crop       = $dimensions['crop'];
 			}
 
-			$result[] = [
-				'breakpoint' => $breakpoint,
-				'width'      => $width,
-				'height'     => $height,
-				'ratio'      => $ratio,
-				'crop'       => $crop
-			];
-		}
-
-		usort($result, ['RP_Breakpoints', 'sort_by_breakpoint']);
-
-		return $result;
-	}
-
-	// this processes our resizing syntax and returns a normalized array with resizing rules
-	public function get_image_rules($input, $img_crop = null) {
-		$variants = ResponsivePics()->breakpoints->add_missing_breakpoints(explode(',', $input));
-		$result   = [];
-
-		foreach ($variants as $variant) {
-			$variant    = trim($variant);
-			$ratio      = null;
-			$crop       = false;
-			$breakpoint = -1;
-			$dimensions = [
-				'width'  => -1,
-				'height' => -1,
-				'crop'   => false
-			];
-
-			if (ResponsivePics()->helpers->is_art_directed($variant)) {
-				ResponsivePics()->error->add_error('invalid', sprintf('art directed parameters (height, factor, crop_x, crop_y) are not supported on image sizes: %s', $variant), $variant);
-			}
-
-			// get global img crop positions
-			if ($img_crop) {
-				// check if crop position is set
-				if (ResponsivePics()->helpers->contains($img_crop, '|')) {
-					$components = explode('|', $img_crop);
-					$ratio      = trim($components[0]);
-					$crop_pos   = $components[1];
-
-					// check if ratio is within range
-					if (ResponsivePics()->process->process_ratio($ratio)) {
-						$crop     = ResponsivePics()->process->process_crop($crop_pos);
-						$variant .= '/'. $ratio;
-					}
-				// add default crop position
-				} else {
-					$ratio        = $img_crop;
-					$default_crop = 'c';
-
-					// check if ratio is within range
-					if (ResponsivePics()->process->process_ratio($ratio)) {
-						$crop     = ResponsivePics()->process->process_crop($default_crop);
-						$variant .= '/'. $ratio;
-					}
-				}
-			}
-
-			// get dimensions
-			if (ResponsivePics()->helpers->contains($variant, ':')) {
-				$components = explode(':', $variant);
-				$breakpoint = ResponsivePics()->process->process_breakpoint($components[0]);
-				$dimensions = ResponsivePics()->process->process_dimensions($components[1]);
-			} else {
-				$dimensions = ResponsivePics()->process->process_dimensions($variant);
-			}
-
-			if (is_array($dimensions)) {
-				$width  = $dimensions['width'];
-				$height = $dimensions['height'];
-				$ratio  = $dimensions['ratio'];
-			}
-
-			if ($breakpoint === -1) {
-				if (ResponsivePics()->helpers->contains($dimensions['input'], '-')) {
-					// use breakpoint based on defined column size
-					$components = explode('-', $dimensions['input']);
-					$bp         = trim($components[0]);
-					$breakpoint = self::$breakpoints[$bp];
-				} else {
-					// use breakpoint based on width
-					$breakpoint = $width;
-				}
+			if (!$art_direction && $global_crop) {
+				$ratio = $global_crop['ratio'];
+				$crop  = $global_crop['crop'];
 			}
 
 			$result[] = [
