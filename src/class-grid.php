973864804176366b068e61236cc0112fe70a40e5
Twansparant <toinekamps@gmail.com>
2020-11-16 14:07:43 +0100

    Use full grid-width for 'full' shorthand

--- src/class-grid.php
+++ src/class-grid.php
@@ -8,14 +8,21 @@ class RP_Grid extends ResponsivePics {
 		$key  = $comp[0];
 		$col  = $comp[1];
 
-		// convert 'xs-full' shorthand to 'xs-12-full'
-		if (ResponsivePics()->helpers->match($col, '/^full/')) {
-			$col = str_replace('full', self::$columns . '-full', $col);
-		}
-
 		// check breakpoint
 		$breakpoint = ResponsivePics()->process->process_breakpoint($key);
 		if (is_numeric($breakpoint)) {
+			// use max breakpoint if there's no bigger one left
+			$next_breakpoint = ResponsivePics()->breakpoints->get_next_breakpoint($key);
+			if (!isset($next_breakpoint)) {
+				$next_breakpoint = $key;
+			}
+
+			// get full width from next breakpoint
+			$grid_width = self::$breakpoints[$next_breakpoint];
+			if (!isset($grid_width)) {
+				ResponsivePics()->error->add_error('missing', sprintf('no breakpoint set for %s', $breakpoint), self::$breakpoints);
+			}
+
 			// strip of ratio
 			if (ResponsivePics()->helpers->contains($col, '/')) {
 				$comp = explode('/', $col);
@@ -28,15 +35,13 @@ class RP_Grid extends ResponsivePics {
 				$col  = trim($comp[0]);
 			}
 
+			// check for full-width syntax (eg. md-full)
+			if (ResponsivePics()->helpers->match($col, '/^full/')) {
+				return $grid_width;
+			}
+
 			// check for full-width syntax (eg. md-6-full)
 			if (ResponsivePics()->helpers->match($col, '/-full$/')) {
-				$next_breakpoint = ResponsivePics()->breakpoints->get_next_breakpoint($key);
-
-				// use max breakpoint if there's no bigger one left
-				if (!isset($next_breakpoint)) {
-					$next_breakpoint = $key;
-				}
-
 				// get full-width columns
 				$comp = explode('-', $col, 2);
 				$col  = $comp[0];
@@ -48,22 +53,10 @@ class RP_Grid extends ResponsivePics {
 					}
 				}
 
-				// get column size from next breakpoint width
-				$grid_width = self::$breakpoints[$next_breakpoint];
-				if (!isset($grid_width)) {
-					ResponsivePics()->error->add_error('missing', sprintf('no breakpoint set for %s', $breakpoint), self::$breakpoints);
-				}
-
 			// normal column syntax (eg. md-6)
 			} else if (ResponsivePics()->helpers->match($col, '/(\d+)/')) {
 				if ($col < 1 || $col > self::$columns) {
 					ResponsivePics()->error->add_error('invalid', sprintf('number of columns %d should be between 1 and %s', $col, self::$columns), $col);
-				} else {
-					// get column size from grid width
-					$grid_width = self::$grid_widths[$key];
-					if (!isset($grid_width)) {
-						ResponsivePics()->error->add_error('missing', sprintf('no width found for breakpoint %s', $breakpoint), self::$grid_widths);
-					}
 				}
 			} else {
 				$grid_width = -1;
