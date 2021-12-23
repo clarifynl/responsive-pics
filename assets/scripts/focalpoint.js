24eaf6c6d8c66f7547c6f44568103a7888182d43
Twansparant <toinekamps@gmail.com>
2021-12-23 14:36:33 +0100

    Build

--- assets/scripts/focalpoint.js
+++ assets/scripts/focalpoint.js
@@ -59,7 +59,7 @@
 			},
 			change: function() {
 				const compat = this.model.get('compat');
-				console.log('change', $(compat.item).find('.compat-field-responsive_pics_focal_point_x'));
+				console.log('change', $(compat.item).find('.compat-field-responsive_pics_focal_point_x input').val(), $(compat.item).find('.compat-field-responsive_pics_focal_point_y input').val());
 				// Re-init Focal Point for images
 				const { type } = this.model.attributes;
 				if (type === 'image') {
