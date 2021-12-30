eb9793f5c120343a23389a45edcdaa05b1644009
Twansparant <toinekamps@gmail.com>
2021-12-30 16:23:24 +0100

    Debug

--- assets/scripts/focalpoint.js
+++ assets/scripts/focalpoint.js
@@ -12,6 +12,7 @@
 		};
 
 		const startDragFocalPoint = e => {
+			$(body).off('dragover.wp-uploader');
 			$imageFocal.addClass('is-dragging');
 			e.originalEvent.dataTransfer.effectAllowed = 'none';
 		};
