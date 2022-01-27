f4738d072f630e0743e15d97144994e87cff6a92
Twansparant <toinekamps@gmail.com>
2022-01-27 09:49:50 +0100

    Use global $as3cf

--- src/class-s3-offload.php
+++ src/class-s3-offload.php
@@ -2,27 +2,12 @@
 
 class RP_S3_Offload extends ResponsivePics {
 	/**
-	 * @var $as3cf
-	 */
-	private $as3cf;
-
-	/**
-	 * Construct S3 Offload
-	 */
-	public function __construct() {
-		global $as3cf;
-
-		if (!empty($as3cf)) {
-			$this->as3cf = $as3cf;
-		}
-	}
-
-	/**
 	 * Upload to S3 storage
 	 */
 	public static function upload_image($id, $file = null) {
-		syslog(LOG_DEBUG, '$this->as3cf: ' . json_encode($this->as3cf));
-		$uploaded = $this->as3cf->upload_attachment($id);
+		global $as3cf;
+		syslog(LOG_DEBUG, '$as3cf: ' . json_encode($as3cf));
+		$uploaded = $as3cf->upload_attachment($id);
 
 		if (is_wp_error($uploaded) || empty($uploaded) || !is_array($uploaded)) {
 			syslog(LOG_DEBUG, json_encode($uploaded));
