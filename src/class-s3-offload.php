2e19409c6e24fef5854300ede5f46d96f3b7f7e3
Twansparant <toinekamps@gmail.com>
2022-01-27 09:35:56 +0100

    Set private $as3cf

--- src/class-s3-offload.php
+++ src/class-s3-offload.php
@@ -2,19 +2,25 @@
 
 class RP_S3_Offload extends ResponsivePics {
 	/**
+	 * @var $as3cf
+	 */
+	private $as3cf;
+
+	/**
 	 * Construct S3 Offload
 	 */
 	public function __construct() {
 		global $as3cf;
-		syslog(LOG_DEBUG, 'RP_S3_Offload $GLOBALS: ' . json_encode($as3cf));
+		if (!empty($as3cf_compat_check)) {
+			$this->as3cf = $as3cf;
+		}
 	}
 
 	/**
 	 * Upload to S3 storage
 	 */
 	public static function upload_image($id, $file = null) {
-		global $as3cf;
-		$uploaded = $as3cf->upload_attachment($id);
+		$uploaded = $this->as3cf->upload_attachment($id);
 
 		if (is_wp_error($uploaded) || empty($uploaded) || !is_array($uploaded)) {
 			syslog(LOG_DEBUG, json_encode($uploaded));
