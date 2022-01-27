3cc099a157c958af21933c0f96dfd3679c58652b
Twansparant <toinekamps@gmail.com>
2022-01-27 09:06:51 +0100

    Debug

--- src/class-s3-offload.php
+++ src/class-s3-offload.php
@@ -10,7 +10,7 @@ class RP_S3_Offload extends ResponsivePics {
 	 * Construct S3 Offload
 	 */
 	public function __construct() {
-		//
+		syslog(LOG_DEBUG, 'RP_S3_Offload $GLOBALS: ' . json_encode($GLOBALS));
 	}
 
 	/**
