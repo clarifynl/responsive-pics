d67a0f249593a8ffd785f31514712c749a6405ab
Twansparant <toinekamps@gmail.com>
2022-01-27 13:12:37 +0100

    Add error on offload error

--- src/class-s3-offload.php
+++ src/class-s3-offload.php
@@ -8,8 +8,12 @@ class RP_S3_Offload extends ResponsivePics {
 		global $as3cf;
 		$s3_upload = $as3cf->upload_attachment($id, null, $file);
 
+		// Check for errors
 		if (is_wp_error($s3_upload) || empty($s3_upload) || !is_array($s3_upload)) {
-			syslog(LOG_DEBUG, json_encode($s3_upload));
+			$error_message = $s3_upload->get_error_message();
+			$error_data    = $s3_upload->get_error_data();
+
+			ResponsivePics()->error->add_error('error', $error_message, $error_data);
 		}
 
 		return $s3_upload;
