efc191eae3ee387241f9753a02d419023b494434
Twansparant <toinekamps@gmail.com>
2022-04-20 12:37:54 +0200

    Change order

--- src/class-rest-api.php
+++ src/class-rest-api.php
@@ -3,10 +3,10 @@
 class RP_Rest_Api extends ResponsivePics {
 
 	public static function register_api_routes() {
-		// picture
-		register_rest_route('responsive-pics/v1', '/get(-picture)?/(?P<id>\d+)', [
+		// image
+		register_rest_route('responsive-pics/v1', '/get-image/(?P<id>\d+)', [
 			'methods'             => 'GET',
-			'callback'            => ['RP_Rest_Api', 'rest_get_picture'],
+			'callback'            => ['RP_Rest_Api', 'rest_get_image'],
 			'permission_callback' => '__return_true',
 			'args'                => [
 				'id' => [
@@ -20,10 +20,10 @@ class RP_Rest_Api extends ResponsivePics {
 			]
 		]);
 
-		// image
-		register_rest_route('responsive-pics/v1', '/get-image/(?P<id>\d+)', [
+		// picture
+		register_rest_route('responsive-pics/v1', '/get(-picture)?/(?P<id>\d+)', [
 			'methods'             => 'GET',
-			'callback'            => ['RP_Rest_Api', 'rest_get_image'],
+			'callback'            => ['RP_Rest_Api', 'rest_get_picture'],
 			'permission_callback' => '__return_true',
 			'args'                => [
 				'id' => [
@@ -55,47 +55,10 @@ class RP_Rest_Api extends ResponsivePics {
 		]);
 	}
 
-	// get picture
-	public static function rest_get_picture($request) {
-		$id        = isset($request['id']) ? $request['id'] : null;
-		$params    = $request->get_params();
-		$sizes     = isset($params['sizes']) ? urldecode($params['sizes']) : null;
-		$classes   = isset($params['classes']) ? urldecode($params['classes']) : null;
-		$lazyload  = isset($params['lazyload']) ? ($params['lazyload'] === 'true') : null;
-		$intrinsic = isset($params['intrinsic']) ? ($params['intrinsic'] === 'true') : null;
-
-		if (class_exists('ResponsivePics')) {
-			if ($sizes) {
-				$picture = ResponsivePics::get_picture($id, $sizes, $classes, $lazyload, $intrinsic);
-
-				// Check for errors
-				if (is_wp_error($picture)) {
-					return $picture;
-				} else {
-					$result = new WP_REST_Response($picture, 200);
-
-					// Set cache duration
-					if (self::$wp_rest_cache) {
-						$cache_duration = self::$wp_rest_cache_duration;
-					} else {
-						$cache_duration = 0;
-					}
-
-					$result->set_headers(array('Cache-Control' => 'max-age=' . $cache_duration));
-					return $result;
-				}
-			} else {
-				return new WP_Error('responsive_pics_invalid',  __('the request is missing the required sizes parameter', 'responsive-pics'), $params);
-			}
-		} else {
-			return new WP_Error('responsive_pics_missing', __('the responsive pics plugin was not found', 'responsive-pics'));
-		}
-	}
-
 	// get image
 	public static function rest_get_image($request) {
-		$id        = isset($request['id']) ? $request['id'] : null;
 		$params    = $request->get_params();
+		$id        = isset($request['id']) ? $request['id'] : null;
 		$sizes     = isset($params['sizes']) ? urldecode($params['sizes']) : null;
 		$classes   = isset($params['classes']) ? urldecode($params['classes']) : null;
 		$crop      = isset($params['crop']) ? urldecode($params['crop']) : null;
@@ -130,10 +93,47 @@ class RP_Rest_Api extends ResponsivePics {
 		}
 	}
 
+	// get picture
+	public static function rest_get_picture($request) {
+		$params    = $request->get_params();
+		$id        = isset($request['id']) ? $request['id'] : null;
+		$sizes     = isset($params['sizes']) ? urldecode($params['sizes']) : null;
+		$classes   = isset($params['classes']) ? urldecode($params['classes']) : null;
+		$lazyload  = isset($params['lazyload']) ? ($params['lazyload'] === 'true') : null;
+		$intrinsic = isset($params['intrinsic']) ? ($params['intrinsic'] === 'true') : null;
+
+		if (class_exists('ResponsivePics')) {
+			if ($sizes) {
+				$picture = ResponsivePics::get_picture($id, $sizes, $classes, $lazyload, $intrinsic);
+
+				// Check for errors
+				if (is_wp_error($picture)) {
+					return $picture;
+				} else {
+					$result = new WP_REST_Response($picture, 200);
+
+					// Set cache duration
+					if (self::$wp_rest_cache) {
+						$cache_duration = self::$wp_rest_cache_duration;
+					} else {
+						$cache_duration = 0;
+					}
+
+					$result->set_headers(array('Cache-Control' => 'max-age=' . $cache_duration));
+					return $result;
+				}
+			} else {
+				return new WP_Error('responsive_pics_invalid',  __('the request is missing the required sizes parameter', 'responsive-pics'), $params);
+			}
+		} else {
+			return new WP_Error('responsive_pics_missing', __('the responsive pics plugin was not found', 'responsive-pics'));
+		}
+	}
+
 	// get background
 	public static function rest_get_background($request) {
-		$id      = isset($request['id']) ? $request['id'] : null;
 		$params  = $request->get_params();
+		$id      = isset($request['id']) ? $request['id'] : null;
 		$sizes   = isset($params['sizes']) ? urldecode($params['sizes']) : null;
 		$classes = isset($params['classes']) ? urldecode($params['classes']) : null;
 
