17bcc7d5cc3421650b9be6ae875749b1e07338ce
Twansparant <toinekamps@gmail.com>
2022-10-03 15:23:17 +0200

    Add /image-sources/ rest route

--- src/class-rest-api.php
+++ src/class-rest-api.php
@@ -7,11 +7,27 @@ class RP_Rest_Api extends ResponsivePics
 	 */
 	public static function register_api_routes() {
 		/**
-		 * Register get-image api route
+		 * Register image api routes
 		 */
 		register_rest_route('responsive-pics/v1', '/(get-)?image/(?P<id>\d+)', [
 			'methods'             => 'GET',
 			'callback'            => ['RP_Rest_Api', 'rest_get_image'],
+			'permission_callback' => '__return_true',
+			'args'                => [
+				'id' => [
+					'validate_callback' => function($id, $request, $key) {
+						return is_numeric($id);
+					},
+					'sanitize_callback' => function($id, $request, $key) {
+						return (int) $id;
+					}
+				]
+			]
+		]);
+
+		register_rest_route('responsive-pics/v1', '/image-sources/(?P<id>\d+)', [
+			'methods'             => 'GET',
+			'callback'            => ['RP_Rest_Api', 'rest_get_image_sources'],
 			'permission_callback' => '__return_true',
 			'args'                => [
 				'id' => [
@@ -87,6 +103,52 @@ class RP_Rest_Api extends ResponsivePics
 		if (class_exists('ResponsivePics')) {
 			if ($sizes) {
 				$image  = ResponsivePics::get_image($id, $sizes, $crop, $classes, $lazyload, $lqip, $route_url);
+
+				// Check for errors
+				if (is_wp_error($image)) {
+					return $image;
+				} else {
+					$result = new WP_REST_Response($image, 200);
+
+					// Set cache duration
+					if (self::$wp_rest_cache) {
+						$cache_duration = self::$wp_rest_cache_duration;
+					} else {
+						$cache_duration = 0;
+					}
+
+					$result->set_headers(array('Cache-Control' => 'max-age=' . $cache_duration));
+					return $result;
+				}
+			} else {
+				return new WP_Error('responsive_pics_invalid',  __('the request is missing the required sizes parameter', 'responsive-pics'), $params);
+			}
+		} else {
+			return new WP_Error('responsive_pics_missing', __('the responsive pics plugin was not found', 'responsive-pics'));
+		}
+	}
+
+
+	/**
+	 * REST API get image sources
+	 *
+	 * @param   (object) WP_REST_Request
+	 * @return  (object) image sources
+	 */
+	public static function rest_get_image_sources($request) {
+		$route        = $request->get_route();
+		$params       = $request->get_params();
+		$query_string = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : null;
+		$route_url    = $query_string ? $route .'?'. $query_string : $route;
+
+		// Decode Parameters
+		$id           = isset($request['id']) ? $request['id'] : null;
+		$sizes        = isset($params['sizes']) ? urldecode($params['sizes']) : null;
+		$crop         = isset($params['crop']) ? urldecode($params['crop']) : null;
+
+		if (class_exists('ResponsivePics')) {
+			if ($sizes) {
+				$image  = ResponsivePics::get_image_sources($id, $sizes, $crop, $route_url);
 
 				// Check for errors
 				if (is_wp_error($image)) {
