f024ff4f25a4c93fb8b88a2a4667af18cb8fff06
Twansparant <toinekamps@gmail.com>
2022-11-11 20:55:44 +0100

    Set default value for $rest_route

--- src/class-process.php
+++ src/class-process.php
@@ -455,7 +455,7 @@ class RP_Process extends ResponsivePics
 	}
 
 	// process the scheduled resize action
-	public static function process_resize_request($id, $quality, $width, $height, $crop, $ratio, $resize_path, $rest_route) {
+	public static function process_resize_request($id, $quality, $width, $height, $crop, $ratio, $resize_path, $rest_route = '') {
 		$file_path = get_attached_file($id);
 		$meta_data = wp_get_attachment_metadata($id);
 		$wp_editor = wp_get_image_editor($file_path);
